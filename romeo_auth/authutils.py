2d999d8f15831f284e24ea152a3443559c53b454
Harry Reeder <harry@harryreeder.co.uk>
2014-10-10 20:25:58 +0000

    Add KeyCAPTCHA

--- romeo_auth/authutils.py
+++ romeo_auth/authutils.py
@@ -1,6 +1,9 @@
 from flask import current_app, redirect, flash, request, abort
 from flask.ext.login import current_user
 from functools import wraps
+from random import randint
+from hashlib import md5
+from urllib import urlopen
 
 
 def groups_required(filter_function):
@@ -38,3 +41,48 @@ def api_key_required(func):
 		else:
 			return func(*args, **kwargs)
 	return decorated_view
+
+KeyCAPTCHA_Template = '''<!-- KeyCAPTCHA code (www.keycaptcha.com)-->
+<input id="capcode" type="hidden" name="capcode" value="123" />
+<script language="JavaScript">
+    var s_s_c_user_id = '#kc_user_id#';
+    var s_s_c_session_id = '#kc_session_id#';
+    var s_s_c_captcha_field_id = 'capcode';
+    var s_s_c_submit_button_id = 'postbut';
+    var s_s_c_web_server_sign = '#kc_s1#';
+    var s_s_c_web_server_sign2 = '#kc_s2#';
+</script>
+<script language=JavaScript src="https://backs.keycaptcha.com/swfs/cap.js"></script>
+<!-- end of KeyCAPTCHA code-->
+'''
+
+def show_keycaptcha(remote_ip='127.0.0.1') :
+  KeyCAPTCHA_PrivateKey = current_app.config['keycaptcha']['secret']
+  KeyCAPTCHA_UserID = current_app.config['keycaptcha']['userid']
+  session_id = str(randint(100000000,999999999999))
+  s1 = md5( session_id + remote_ip + KeyCAPTCHA_PrivateKey ).hexdigest()
+  s2 = md5( session_id + KeyCAPTCHA_PrivateKey ).hexdigest()
+  rd = { 'kc_user_id':KeyCAPTCHA_UserID, 'kc_s1':s1, 'kc_s2':s2, 'kc_session_id':session_id }
+  st = KeyCAPTCHA_Template
+  for k in rd.keys() :
+    st = st.replace( '#' + k + '#', rd[k] )
+  return st
+
+
+def validate_keycaptcha( capcode ) :  
+  KeyCAPTCHA_PrivateKey = current_app.config['keycaptcha']['secret']
+  KeyCAPTCHA_UserID = current_app.config['keycaptcha']['userid']
+  cap = capcode.split('|')
+  if len( cap ) < 3 : return False
+  valid_cap_sign = md5( 'accept' + cap[1] + KeyCAPTCHA_PrivateKey + cap[2] ).hexdigest()
+  if valid_cap_sign <> cap[0] : return False
+  if cap[2].find( 'http://' ) == 0 : 
+    try :
+      f = urlopen( cap[2] + cap[1] )
+      st = f.read()
+      f.close()
+    except :
+      return False
+    if st <> '1' : return False
+  else : return False
+  return True
--- romeo_auth/views/registration.py
+++ romeo_auth/views/registration.py
@@ -1,14 +1,21 @@
 from romeo_auth import app, ldaptools
+from romeo_auth.authutils import show_keycaptcha, validate_keycaptcha
 from flask import render_template, request, flash, redirect
 from flask.ext.login import login_user
 from ldap import ALREADY_EXISTS
 
 @app.route('/signup')
 def signup():
-	return render_template("signup.html")
+	captcha = show_keycaptcha(request.remote_addr)
+	return render_template("signup.html", captcha=captcha)
 
 @app.route('/create_account', methods=['POST'])
 def create_account():
+	capcode = request.form.get("capcode")
+	if not validate_keycaptcha(capcode):
+		flash("Captcha Incorrect, Please Try Again")
+		return redirect("/signup")
+
 	attrs = {}
 	attrs["uid"] = request.form.get("username")
 	attrs["email"] = request.form.get("email")
